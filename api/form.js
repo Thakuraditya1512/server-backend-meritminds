dd009392bc87b361b0ee74ec5a92e7df985fba0c
LearningDestiny <devabathinipranavchowdary@gmail.com>
2024-10-22 14:07:51 +0530

    changed form again

--- api/form.js
+++ api/form.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config(); // Load .env file
+require('dotenv').config(); // Load environment variables from .env
 
 const app = express();
 app.use(cors());
